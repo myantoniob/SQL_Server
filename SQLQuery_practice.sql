dda80a121cef9102bbd661e817ebfe0f395657e7
Mynor Ramirez <myantoniob@gmail.com>
2023-12-05 22:52:54 -0600

    comments on code

--- SQLQuery_practice.sql
+++ SQLQuery_practice.sql
@@ -1,19 +1,33 @@
 -- Basic query
+-- Fue utilizada la DB de pruebas #Northwind
 USE Northwind
 
+/*
+Utiliza la tabla #Customer,
+Filtra solo las #Country que sean USA or MEXICO
+y muestra los distintos campo del SELECT
+y los ordena por el country
+*/
 SELECT CustomerID, CompanyName, ContactTitle, [Address], Country 
 FROM Customers
 WHERE Country IN ('usa', 'mexico')
 ORDER BY Country		
 GO
 
-
+/*
+Utiliza la tabla # Customers
+Los agrupa por Country
+HAVING lo filtra al mostrar solo los grupo mayores a 10
+los ordena por Country
+muestra 2 columnas, Country y cuantas veces se repite
+*/
 SELECT country, COUNT(*) AS [Counted]
 FROM Customers
 GROUP BY Country
 HAVING COUNT(*) > 10
 ORDER BY Country
 GO
+
 
 SELECT Productname, unitprice FROM Products
 WHERE unitprice BETWEEN 15 AND 20
@@ -34,6 +48,12 @@ GO
 
 
 -- Consulta de mas de dos tablas
+
+/*
+Utilizando las Tablas #Customer, #Orders, #Products y #Order Details
+Se relacionan con INNER JOIN, solo los valores que tiene relacion, Implica no NUllS
+
+*/
 SELECT C.CustomerID, C.CompanyName, C.ContactName, C.Country, 
 O.OrderID, O.OrderDate, 
 P.ProductName,
@@ -46,7 +66,11 @@ INNER JOIN Products AS P
 ON D.ProductID = P.ProductID
 GO
 
-
+/*
+Utilizando las tablas #Customers y #Orders
+LEFT OUTER JOIN, se mostraran solo los valores de #Customers que no tengan
+coneccion con la #Orders
+*/
 SELECT C.CustomerID, C.CompanyName, C.ContactName, C.Country,
  O.OrderID, O.OrderDate
  FROM Customers AS C LEFT OUTER JOIN Orders AS O
@@ -59,7 +83,9 @@ SELECT C.CustomerID, C.CompanyName, C.ContactName, C.Country,
 USE Northwind
 GO
 
-
+/*
+Se crea una relacion con las misma Tabla, solo las que tenga una relacion
+*/
 SELECT J.FirstName +' '+J.LastName AS JEFE, 
 S.FirstName +' '+S.LastName AS SUBALTERNO
 FROM Employees AS J INNER JOIN Employees AS S
@@ -67,6 +93,10 @@ ON J.EmployeeID = S.ReportsTo
 ORDER BY J.FirstName
 GO
 
+/*
+Muestra todos los balores de la primera Tabla,
+los que tiene relacion como los que no
+*/
 SELECT J.FirstName +' '+J.LastName AS JEFE, 
 S.FirstName +' '+S.LastName AS SUBALTERNO
 FROM Employees AS J LEFT OUTER JOIN Employees AS S
